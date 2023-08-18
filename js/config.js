69f646b969e49879002c75020f15ac83a2e17c85
Ton Zijlstra <tonzyl@users.noreply.github.com>
2023-08-18 08:52:32 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -65,10 +65,10 @@ let respecConfig = {
   //neem hier de URL van de github repository op waar het respec document in staat
   github: "https://github.com/tonzyl/ourcommondataspace",
   // Create PDF and link to file in header (optional):
-  alternateFormats: [
-      {
-          label: "pdf",
-          uri: "template.pdf",
-      },
-  ],
+  //alternateFormats: [
+  //    {
+  //        label: "pdf",
+  //        uri: "template.pdf",
+  //    },
+  //],
 };
